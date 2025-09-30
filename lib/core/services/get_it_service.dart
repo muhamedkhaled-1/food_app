5cad22cdfc912f1ef940fb6399220bd23a1a34ea
Muhamed khaled <muhamedflutter@gmail.com>
2025-10-01 02:53:03 +0300

    get it service

---
+++ lib/core/services/get_it_service.dart
@@ -0,0 +1,13 @@
+import 'package:food_app/core/services/firebase_auth_services.dart';
+import 'package:food_app/mobile_layout/features/auth/data/repos/auth_repo_impl.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setup() {
+  getIt.registerSingleton<FirebaseAuthServices>(FirebaseAuthServices());
+  getIt.registerSingleton<AuthRepoImpl>(AuthRepoImpl(
+      firebaseAuthServices: getIt<FirebaseAuthServices>()));
+
+
+}
--- lib/mobile_layout/features/auth/presentation/views/sign_up_view.dart
+++ lib/mobile_layout/features/auth/presentation/views/sign_up_view.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:food_app/constants.dart';
+import 'package:food_app/core/services/get_it_service.dart';
+import 'package:food_app/mobile_layout/features/auth/domain/repos/auth_repo.dart';
+import 'package:food_app/mobile_layout/features/auth/presentation/cubits/signup_cubit/signup_cubit.dart';
 import 'package:food_app/mobile_layout/features/auth/presentation/views/widgets/sign_up_view_body.dart';
 
 class SignUpView extends StatelessWidget {
   const SignUpView({super.key});
-  static const routeName='signUp';
+
+  static const routeName = 'signUp';
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: KbackColor,
-      body: SignUpViewBody(),
+    return BlocProvider(
+      create: (context) => SignupCubit(getIt<AuthRepo>()),
+      child: Scaffold(
+        backgroundColor: KbackColor,
+        body: SignUpViewBody(),
+      ),
     );
   }
 }
